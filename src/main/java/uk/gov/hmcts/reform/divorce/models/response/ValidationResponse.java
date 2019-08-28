81ac874f2c1cbf85747c57a280b843f2c60168b8
Patryk Jar <jar.patryk@gmail.com>
2019-08-28 11:46:25 +0200

    DIV-4454: add rules config

--- src/main/java/uk/gov/hmcts/reform/divorce/models/response/ValidationResponse.java
+++ src/main/java/uk/gov/hmcts/reform/divorce/models/response/ValidationResponse.java
@@ -1,5 +1,6 @@
 package uk.gov.hmcts.reform.divorce.models.response;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Builder;
 import lombok.Data;
@@ -16,4 +17,12 @@ public class ValidationResponse {
     private List<String> errors;
 
     private List<String> warnings;
+
+    @JsonIgnore
+    public boolean isValid() {
+        boolean hasNoErrors = this.getErrors() == null || this.getErrors().isEmpty();
+        boolean hasNoWarnings = this.getWarnings() == null || this.getWarnings().isEmpty();
+
+        return hasNoErrors && hasNoWarnings;
+    }
 }
