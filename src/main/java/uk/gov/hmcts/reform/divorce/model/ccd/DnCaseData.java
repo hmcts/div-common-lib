144578cb755c745c24d39440501cd0bf52691433
tony.chow <tony.chow@hmcts.net>
2019-10-16 07:48:52 +0100

    Removed extra fields from DnCaseData

--- src/main/java/uk/gov/hmcts/reform/divorce/model/ccd/DnCaseData.java
+++ src/main/java/uk/gov/hmcts/reform/divorce/model/ccd/DnCaseData.java
@@ -1,7 +1,6 @@
 package uk.gov.hmcts.reform.divorce.model.ccd;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
@@ -90,42 +89,4 @@ public class DnCaseData extends DnRefusalCaseData {
 
     @JsonProperty("DesertionAskedToResumeDNDetails")
     private String desertionAskedToResumeDNDetails;
-
-    // These three JsonInclude are ignored for now until DN Refusal Config is in Production
-    @JsonInclude(JsonInclude.Include.NON_NULL)
-    @JsonProperty("DnClarificationResponse")
-    private List<CollectionMember<String>> dnClarificationResponse;
-
-    @JsonInclude(JsonInclude.Include.NON_NULL)
-    @JsonProperty("DnClarificationUploadDocuments")
-    private List<CollectionMember<String>> dnClarificationUploadDocuments;
-
-    @JsonInclude(JsonInclude.Include.NON_NULL)
-    @JsonProperty("DocumentsUploadedDnClarification")
-    private List<CollectionMember<Document>> documentsUploadedDnClarification;
-
-    // Caseworker only fields so frontend submission does not modify these
-    @JsonInclude(JsonInclude.Include.NON_NULL)
-    @JsonProperty("RefusalClarificationReason")
-    private List<String> refusalClarificationReason;
-
-    @JsonInclude(JsonInclude.Include.NON_NULL)
-    @JsonProperty("RefusalClarificationAdditionalInfo")
-    private String refusalClarificationAdditionalInfo;
-
-    @JsonInclude(JsonInclude.Include.NON_NULL)
-    @JsonProperty("RefusalRejectionReason")
-    private List<String> refusalRejectionReason;
-
-    @JsonInclude(JsonInclude.Include.NON_NULL)
-    @JsonProperty("RefusalRejectionAdditionalInfo")
-    private String refusalRejectionAdditionalInfo;
-
-    @JsonInclude(JsonInclude.Include.NON_NULL)
-    @JsonProperty("RefusalAdminErrorInfo")
-    private String refusalAdminErrorInfo;
-
-    @JsonInclude(JsonInclude.Include.NON_NULL)
-    @JsonProperty("DnOutcomeCase")
-    private String dnOutcomeCase;
 }
