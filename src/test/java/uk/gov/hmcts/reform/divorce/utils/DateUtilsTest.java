9574041a68b81b1ce693048e1fd9b63d68fedfe4
Patryk Jar <jar.patryk@gmail.com>
2020-06-09 11:27:25 +0200

    1.1.23: one way of formatting dates for emails and docs - DateUtils.formatDateWithCustomerFacingFormat

--- src/test/java/uk/gov/hmcts/reform/divorce/utils/DateUtilsTest.java
+++ src/test/java/uk/gov/hmcts/reform/divorce/utils/DateUtilsTest.java
@@ -17,7 +17,6 @@ import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertThat;
-import static uk.gov.hmcts.reform.divorce.utils.DateUtils.formatDateForDocuments;
 import static uk.gov.hmcts.reform.divorce.utils.DateUtils.formatDateTimeForCcd;
 import static uk.gov.hmcts.reform.divorce.utils.DateUtils.formatDateWithCustomerFacingFormat;
 
@@ -46,7 +45,7 @@ public class DateUtilsTest {
 
     @Test
     public void givenDate_whenFormatDateIsCalled_thenDateStringIsReturned() {
-        LocalDateTime localDate = LocalDate.of(2000, 01, 01).atStartOfDay();
+        LocalDateTime localDate = LocalDate.of(2000, 1, 1).atStartOfDay();
         Instant instant = localDate.toInstant(ZoneOffset.UTC);
         Date date = Date.from(instant);
 
@@ -58,7 +57,7 @@ public class DateUtilsTest {
 
     @Test
     public void givenInstant_whenFormatDateIsCalled_thenDateStringIsReturned() {
-        LocalDateTime localDate = LocalDate.of(2000, 01, 01).atStartOfDay();
+        LocalDateTime localDate = LocalDate.of(2000, 1, 1).atStartOfDay();
         Instant instant = localDate.toInstant(ZoneOffset.UTC);
 
         String expectedDateString = "2000-01-01";
@@ -69,7 +68,7 @@ public class DateUtilsTest {
 
     @Test
     public void givenLocalDate_whenFormatDateFromLocalDateIsCalled_thenDateStringIsReturned() {
-        LocalDate localDate = LocalDate.of(2000, 01, 01)
+        LocalDate localDate = LocalDate.of(2000, 1, 1)
             .atStartOfDay()
             .toLocalDate();
 
@@ -81,7 +80,7 @@ public class DateUtilsTest {
 
     @Test
     public void givenLocalDateTime_whenFormatDateFromDateTimeIsCalled_thenDateStringIsReturned() {
-        LocalDateTime localDate = LocalDate.of(2000, 01, 01).atStartOfDay();
+        LocalDateTime localDate = LocalDate.of(2000, 1, 1).atStartOfDay();
 
         String expectedDateString = "2000-01-01";
         String dateString = DateUtils.formatDateFromDateTime(localDate);
@@ -92,7 +91,7 @@ public class DateUtilsTest {
     @Test
     public void givenLocalDateTime_whenFormatTimeFromDateTimeIsCalled_thenDateStringIsReturned() {
         LocalDateTime localDate = LocalDate
-            .of(2000, 01, 01)
+            .of(2000, 1, 1)
             .atTime(12, 34);
 
         String expectedDateString = "12:34";
@@ -130,23 +129,19 @@ public class DateUtilsTest {
     }
 
     @Test
-    public void formatDateForDocumentsReturnsFormattedDateStringLeadingZerosForDay() {
+    public void formatDateWithCustomerFacingFormatStringReturnsFormattedDateString() {
         assertThat(
-            formatDateForDocuments(LocalDate.of(2020, Month.JUNE, 4)),
-            is("04 Jun 2020")
+            formatDateWithCustomerFacingFormat("2020-06-04"),
+            is("4 June 2020")
         );
-        assertThat(formatDateForDocuments("2020-06-04"), is("04 Jun 2020"));
-        assertThat(
-                formatDateForDocuments(LocalDate.of(1999, Month.OCTOBER, 20)),
-                is("20 Oct 1999")
-        );
-        assertThat(formatDateForDocuments("1999-10-20"), is("20 Oct 1999"));
+        assertThat(formatDateWithCustomerFacingFormat("2020-06-04"), is("4 June 2020"));
+        assertThat(formatDateWithCustomerFacingFormat("1999-10-20"), is("20 October 1999"));
     }
 
     @Test
     public void formatDateTimeForCcdReturnsValidString() {
         assertThat(
-            formatDateTimeForCcd(LocalDateTime.of(2020, Month.JUNE, 4, 2,1,5)),
+            formatDateTimeForCcd(LocalDateTime.of(2020, Month.JUNE, 4, 2, 1, 5)),
             is("2020-06-04T02:01:05.000")
         );
     }
