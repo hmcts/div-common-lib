e91b27488d0c5ea0c862168c21d1de24e4e00b1a
Shaun Ganley <shaung@Fare.local>
2021-04-09 08:48:30 +0100

    DIV-6872: Map respondent organisation ID from CCD to Divorce session

--- src/main/java/uk/gov/hmcts/reform/divorce/model/ccd/Organisation.java
+++ src/main/java/uk/gov/hmcts/reform/divorce/model/ccd/Organisation.java
@@ -3,11 +3,13 @@ package uk.gov.hmcts.reform.divorce.model.ccd;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
+import lombok.Getter;
 import lombok.ToString;
 
 @ToString
 @EqualsAndHashCode
 @AllArgsConstructor
+@Getter
 public class Organisation {
 
     @JsonProperty("OrganisationID")
