27279487a440880a8be052dc0eb26102a449efde
danielbe11 <daniel.bell@solirius.com>
2021-12-22 14:21:40 +0000

    Completed first half of tests

--- src/test/java/uk/gov/hmcts/reform/divorce/validation/rules/d8/D8MarriageDateTest.java
+++ src/test/java/uk/gov/hmcts/reform/divorce/validation/rules/d8/D8MarriageDateTest.java
@@ -2,130 +2,108 @@ package uk.gov.hmcts.reform.divorce.validation.rules.d8;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.test.context.junit4.SpringRunner;
 import uk.gov.hmcts.reform.divorce.model.ccd.CoreCaseData;
 import uk.gov.hmcts.reform.divorce.utils.DateUtils;
 
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
+import java.util.List;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 
-@RunWith(SpringRunner.class)
 public class D8MarriageDateTest {
 
     private D8MarriageDate rule;
     private CoreCaseData coreCaseData;
+    private List<String> result;
 
     @Before
     public void setup() {
         rule = new D8MarriageDate();
         coreCaseData = new CoreCaseData();
+        result = new ArrayList<>();
     }
 
     @Test
     public void whenShouldReturnTrueWhenD8MarriageDateIsNull() {
-        rule.setCoreCaseData(coreCaseData);
-        boolean result = rule.when();
+        result = rule.execute(coreCaseData, result);
 
-        assertEquals(true, result);
+        assertThat(result.isEmpty(), is(false));
     }
 
     @Test
     public void whenShouldReturnTrueWhenD8MarriageDateLessThanOneYearAgo() {
         String d8MarriageDate = DateUtils.formatDate(Instant.now().minus(100, ChronoUnit.DAYS));
         coreCaseData.setD8MarriageDate(d8MarriageDate);
+        result = rule.execute(coreCaseData, result);
 
-        rule.setCoreCaseData(coreCaseData);
-        boolean result = rule.when();
-
-        assertEquals(true, result);
+        assertThat(result.isEmpty(), is(false));
     }
 
     @Test
     public void whenShouldReturnTrueWhenD8MarriageDateMoreThanOneHundredYearsAgo() {
         String d8MarriageDate = DateUtils.formatDate(Instant.now().minus(365 * 105, ChronoUnit.DAYS));
         coreCaseData.setD8MarriageDate(d8MarriageDate);
+        result = rule.execute(coreCaseData, result);
 
-        rule.setCoreCaseData(coreCaseData);
-        boolean result = rule.when();
-
-        assertEquals(true, result);
+        assertThat(result.isEmpty(), is(false));
     }
 
     @Test
     public void whenShouldReturnTrueWhenD8MarriageDateIsInTheFuture() {
         String d8MarriageDate = DateUtils.formatDate(Instant.now().plus(100, ChronoUnit.DAYS));
         coreCaseData.setD8MarriageDate(d8MarriageDate);
+        result = rule.execute(coreCaseData, result);
 
-        rule.setCoreCaseData(coreCaseData);
-        boolean result = rule.when();
-
-        assertEquals(true, result);
+        assertThat(result.isEmpty(), is(false));
     }
 
     @Test
     public void whenShouldReturnFalseWhenD8MarriageDateIsValid() {
         String d8MarriageDate = DateUtils.formatDate(Instant.now().minus(365 * 2, ChronoUnit.DAYS));
         coreCaseData.setD8MarriageDate(d8MarriageDate);
+        result = rule.execute(coreCaseData, result);
 
-        rule.setCoreCaseData(coreCaseData);
-        boolean result = rule.when();
-
-        assertEquals(false, result);
+        assertThat(result.isEmpty(), is(true));
     }
 
     @Test
     public void thenShouldReturnErrorMessageWithNull() {
-        rule.setCoreCaseData(coreCaseData);
+        result = rule.execute(coreCaseData, result);
 
-        rule.setResult(new ArrayList<>());
-        rule.then();
-
-        assertEquals("D8MarriageDate can not be null or empty. Actual data is: null", rule.getResult().get(0));
+        assertEquals("D8MarriageDate can not be null or empty. Actual data is: null", result.get(0));
     }
 
     @Test
     public void thenShouldReturnErrorMessageWhenD8MarriageDateLessThanOneYearAgo() {
         String d8MarriageDate = DateUtils.formatDate(Instant.now().minus(100, ChronoUnit.DAYS));
         coreCaseData.setD8MarriageDate(d8MarriageDate);
-
-        rule.setCoreCaseData(coreCaseData);
-
-        rule.setResult(new ArrayList<>());
-        rule.then();
+        result = rule.execute(coreCaseData, result);
 
         assertEquals("D8MarriageDate can not be less than one year ago. Actual data is: ".concat(d8MarriageDate),
-            rule.getResult().get(0));
+            result.get(0));
     }
 
     @Test
     public void thenShouldReturnErrorMessageWhenD8MarriageDateMoreThanOneHundredYearsAgo() {
         String d8MarriageDate = DateUtils.formatDate(Instant.now().minus(365 * 105, ChronoUnit.DAYS));
         coreCaseData.setD8MarriageDate(d8MarriageDate);
-
-        rule.setCoreCaseData(coreCaseData);
-
-        rule.setResult(new ArrayList<>());
-        rule.then();
+        result = rule.execute(coreCaseData, result);
 
         assertEquals("D8MarriageDate can not be more than 100 years ago. Actual data is: ".concat(d8MarriageDate),
-            rule.getResult().get(0));
+            result.get(0));
     }
 
     @Test
     public void thenShouldReturnErrorMessageWhenD8MarriageDateIsInTheFuture() {
         String d8MarriageDate = DateUtils.formatDate(Instant.now().plus(100, ChronoUnit.DAYS));
         coreCaseData.setD8MarriageDate(d8MarriageDate);
-
-        rule.setCoreCaseData(coreCaseData);
-
-        rule.setResult(new ArrayList<>());
-        rule.then();
+        result = rule.execute(coreCaseData, result);
 
         assertEquals("D8MarriageDate can not be in the future. Actual data is: ".concat(d8MarriageDate),
-            rule.getResult().get(0));
+            result.get(0));
     }
 }
--- src/test/java/uk/gov/hmcts/reform/divorce/validation/rules/d8/D8PetitionerContactDetailsConfidentialTest.java
+++ src/test/java/uk/gov/hmcts/reform/divorce/validation/rules/d8/D8PetitionerContactDetailsConfidentialTest.java
@@ -2,52 +2,47 @@ package uk.gov.hmcts.reform.divorce.validation.rules.d8;
 
 import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.springframework.test.context.junit4.SpringRunner;
 import uk.gov.hmcts.reform.divorce.model.ccd.CoreCaseData;
 
 import java.util.ArrayList;
+import java.util.List;
 
+import static org.hamcrest.CoreMatchers.is;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 
-@RunWith(SpringRunner.class)
 public class D8PetitionerContactDetailsConfidentialTest {
 
     private D8PetitionerContactDetailsConfidential rule;
     private CoreCaseData coreCaseData;
+    private List<String> result;
 
     @Before
     public void setup() {
         rule = new D8PetitionerContactDetailsConfidential();
         coreCaseData = new CoreCaseData();
+        result = new ArrayList<>();
     }
 
     @Test
     public void whenShouldReturnTrueWhenD8PetitionerContactDetailsConfidentialIsNull() {
-        rule.setCoreCaseData(coreCaseData);
-        boolean result = rule.when();
+        result = rule.execute(coreCaseData, result);
 
-        assertEquals(true, result);
+        assertThat(result.isEmpty(), is(false));
     }
 
     @Test
     public void whenShouldReturnFalseWhenD8PetitionerContactDetailsConfidentialIsNotNull() {
         coreCaseData.setD8PetitionerContactDetailsConfidential("Yes");
+        result = rule.execute(coreCaseData, result);
 
-        rule.setCoreCaseData(coreCaseData);
-        boolean result = rule.when();
-
-        assertEquals(false, result);
+        assertThat(result.isEmpty(), is(true));
     }
 
     @Test
     public void thenShouldReturnErrorMessageWithNull() {
-        rule.setCoreCaseData(coreCaseData);
+        result = rule.execute(coreCaseData, result);
 
-        rule.setResult(new ArrayList<>());
-        rule.then();
-
-        assertEquals("D8PetitionerContactDetailsConfidential can not be null or empty. Actual data is: null",
-            rule.getResult().get(0));
+        assertEquals("D8PetitionerContactDetailsConfidential can not be null or empty. Actual data is: null", result.get(0));
     }
 }
