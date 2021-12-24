b2c3e2e32f932b05e55b86f6a8ec2b6f5eb6c743
EllisD-B <ellis.domengerboyce@hmcts.net>
2021-12-24 15:09:07 +0000

    imports

--- src/test/java/uk/gov/hmcts/reform/divorce/validation/rules/d8/D8MarriageRespondentNameTest.java
+++ src/test/java/uk/gov/hmcts/reform/divorce/validation/rules/d8/D8MarriageRespondentNameTest.java
@@ -8,7 +8,9 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.hamcrest.CoreMatchers.is;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
+
 
 public class D8MarriageRespondentNameTest {
 
